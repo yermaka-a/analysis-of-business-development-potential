d3c919bffabc7544bcc64d5d4a9e632684bb3474
yermaka <ermak.080@mail.ru>
2025-03-06 00:38:48 +0800

    update HomePage

--- src/pages/HomePage/HomePage.vue
+++ src/pages/HomePage/HomePage.vue
@@ -2,6 +2,8 @@
 <script setup lang="ts">
 import MainDashboard from '../../components/MainDashboard/MainDashboard.vue';
 import MainInputForm from '../../components/MainInputForm';
+import MainRatingTypes from "@components/RatingTypes"
+import MainSamplingPrinciple from "@components/SamplingPrinciple"
 </script>
 
 <template>
@@ -9,6 +11,10 @@ import MainInputForm from '../../components/MainInputForm';
     <div class="main-container">
     <MainInputForm/>
     <MainDashboard/>
+  </div>
+  <div class="config-container">
+    <MainRatingTypes/>
+    <MainSamplingPrinciple/>
   </div>
   </main>
 </template>
@@ -18,6 +24,12 @@ import MainInputForm from '../../components/MainInputForm';
     .main-container{
       display: grid;
       grid-template-columns: 1fr 2fr;
+      grid-gap: 10%;
+      margin-bottom: 20px;
+    }
+    .config-container{
+      display: grid;
+      grid-template-columns: 1fr 1fr;
       grid-gap: 10%;
     }
 </style>
