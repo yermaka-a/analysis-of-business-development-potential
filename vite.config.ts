67a5d7f3ca6af07be50d3e6e088d43a32504e071
yermaka <ermak.080@mail.ru>
2025-03-06 00:36:56 +0800

    add alias to paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {join} from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: [
+        {find: '@/', replacement: join(__dirname, "src")},
+        {find: '@components', replacement: join(__dirname, "src", "components")},
+        {find: '@public', replacement: join(__dirname, "public")},
+        {find: '@assets', replacement: join(__dirname, "src", "assets")},
+        {find: '@pages', replacement: join(__dirname, "src", "pages")},
+    ],
+},
 })
