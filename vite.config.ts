a84b338806708991b5e510aa3c776a25505ba16e
yermaka <ermak.080@mail.ru>
2025-03-10 00:22:24 +0800

    fix @/  in @

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: [
-        {find: '@/', replacement: join(__dirname, "src")},
+        {find: '@', replacement: join(__dirname, "src")},
         {find: '@components', replacement: join(__dirname, "src", "components")},
         {find: '@public', replacement: join(__dirname, "public")},
         {find: '@assets', replacement: join(__dirname, "src", "assets")},
