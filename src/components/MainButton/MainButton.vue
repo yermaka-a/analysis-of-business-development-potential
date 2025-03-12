36d2b52ff941b79a5a76b58d90785223eb4fd4ee
yermaka <ermak.080@mail.ru>
2025-03-12 23:36:09 +0800

    create MainButton

---
+++ src/components/MainButton/MainButton.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import type { IProps } from './types';
+
+const { disabled, onClick, classes } = defineProps<IProps>()
+</script>
+
+<template>
+  <button @click="onClick" type="button" :class="classes" :disabled=disabled><slot/></button>
+</template>
+
+<style lang="scss" scoped>
+button:hover {
+  background-color: #A50034;
+  /* Красный цвет при наведении */
+}
+
+button {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+button:disabled {
+  background-color: #4188c5; /* Цвет для отключенной кнопки */
+  cursor: not-allowed; /* Изменение курсора для отключенной кнопки */
+}
+button {
+  display: inline;
+  background-color: #0056A0;
+  /* Синий цвет */
+  color: white;
+  padding: 10px 15px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 1rem;
+}
+</style>
---
+++ src/components/MainButton/types.ts
@@ -0,0 +1,5 @@
+export interface IProps {
+  readonly disabled: boolean
+  readonly onClick: () => void
+  readonly classes?: string
+}
