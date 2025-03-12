aba0219ad1cc8956aa5270b0c6e27b3c275ff346
yermaka <ermak.080@mail.ru>
2025-03-12 18:44:15 +0800

    update HomePage

--- src/pages/HomePage/HomePage.vue
+++ src/pages/HomePage/HomePage.vue
@@ -1,35 +1,44 @@
-
 <script setup lang="ts">
-import MainDashboard from '../../components/MainDashboard/MainDashboard.vue';
+import ListOfCompanies from '../../components/ListOfCompanies/ListOfCompanies.vue';
 import MainInputForm from '../../components/MainInputForm';
 import MainRatingTypes from "@components/RatingTypes"
 import MainSamplingPrinciple from "@components/SamplingPrinciple"
 </script>
 
 <template>
-  <main >
+  <main>
     <div class="main-container">
-    <MainInputForm/>
-    <MainDashboard/>
-  </div>
-  <div class="config-container">
-    <MainRatingTypes/>
-    <MainSamplingPrinciple/>
-  </div>
+      <MainInputForm />
+      <ListOfCompanies />
+    </div>
+    <div class="config-container">
+      <MainRatingTypes />
+      <MainSamplingPrinciple />
+    </div>
   </main>
 </template>
 
 
 <style lang="scss" scoped>
-    .main-container{
-      display: grid;
-      grid-template-columns: 1fr 2fr;
-      grid-gap: 10%;
-      margin-bottom: 20px;
-    }
-    .config-container{
-      display: grid;
-      grid-template-columns: 1fr 1fr;
-      grid-gap: 10%;
-    }
+main {
+ display: flex;
+ flex-direction: column;
+ justify-content: center;
+ align-items: center;
+}
+
+.main-container {
+  width: 95%;
+  display: grid;
+  grid-template-columns: 1fr 4fr;
+  gap: 5%;
+  margin-bottom: 20px;
+}
+
+.config-container {
+  width: 95%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 10%;
+}
 </style>
